62fba1e8fc5f128e99a112806c6a2db595c89f8d
Shontae Hogan <shontaehogan@gmail.com>
2017-08-30 10:39:35 -0400

    completed and ran some more tests

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # word_frequency
+
+Calculate the frequency of words in a text file. A starter file with a class and some method names is provided, as is a test suite for your code.
+
+To calculate the frequency of words, you must:
+
+remove punctuation
+normalize all words to lowercase
+remove "stop words" -- words used so frequently they are ignored
+go through the file word by word and keep a count of how often each word is used
+When your program is complete, you should be able to run ruby wordfreq.rb seneca_falls.txt and get a printed report like this:
+
+    her | 33 *********************************
+    all | 12 ************
+  which | 12 ************
+   they | 7  *******
+  their | 7  *******
+    she | 7  *******
+   this | 6  ******
+   them | 6  ******
+  women | 6  ******
+ rights | 6  ******
+The class is located in wordfreq.rb in the 'starterfiles' folder. To run the tests to see if your code is correct, run `ruby wordfreqtest.rb`.
---
+++ wordfreq.rb
@@ -0,0 +1,59 @@
+class Wordfreq
+  STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from',
+    'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with']
+
+  def initialize(filename)
+    contents = File.read(filename).downcase.gsub("--", " ")
+    contents = contents.gsub(/[^a-z0-9\s]/i, "")
+    word_array = contents.split(" ") - STOP_WORDS
+
+    @words = {}
+
+    word_array.each do |word|
+      if @words.include?(word)
+        @words[word] += 1
+      else
+        @words[word] = 1
+      end
+  end
+  top_words(5)
+end
+
+
+
+  def frequency(word)
+    if @words.has_key?(word)
+      return frequencies[word]
+    else
+      0
+  end
+end
+
+  def frequencies
+    @words
+  end
+
+  def top_words(number)
+    @words.sort_by { |word, count| count }.reverse.take(5)
+  end
+  
+
+#   def print_report
+#   end
+# end
+#
+# if __FILE__ == $0
+#   filename = ARGV[0]
+#   if filename
+#     full_filename = File.absolute_path(filename)
+#     if File.exists?(full_filename)
+#       wf = Wordfreq.new(full_filename)
+#       wf.print_report
+#     else
+#       puts "#{filename} does not exist!"
+#     end
+#   else
+#     puts "Please give a filename as an argument."
+#   end
+end
